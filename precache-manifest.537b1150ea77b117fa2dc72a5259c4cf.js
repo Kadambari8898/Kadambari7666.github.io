49da71ac354655d836f0a3f4da6fdf169e65b867
Tejas Bitale <tejasbitale007@gmail.com>
2022-08-18 20:00:30 +0530

    Updates

---
+++ precache-manifest.537b1150ea77b117fa2dc72a5259c4cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "70b4297b04b97d96d8ab",
+    "url": "/Kadambari7666.github.io/static/css/main.3bd27957.chunk.css"
+  },
+  {
+    "revision": "70b4297b04b97d96d8ab",
+    "url": "/Kadambari7666.github.io/static/js/main.70b4297b.chunk.js"
+  },
+  {
+    "revision": "3f760a3e53f5ae50a060",
+    "url": "/Kadambari7666.github.io/static/js/1.3f760a3e.chunk.js"
+  },
+  {
+    "revision": "114373115389260853f0",
+    "url": "/Kadambari7666.github.io/static/js/runtime~main.11437311.js"
+  },
+  {
+    "revision": "f7452439ba426610dd09feb78717fbb5",
+    "url": "/Kadambari7666.github.io/static/media/kadu1.f7452439.jfif"
+  },
+  {
+    "revision": "6015dd37985a596739b55de9da005de2",
+    "url": "/Kadambari7666.github.io/static/media/img3.6015dd37.webp"
+  },
+  {
+    "revision": "ace012df19097b444d83e5d48ee82a2d",
+    "url": "/Kadambari7666.github.io/index.html"
+  }
+];
